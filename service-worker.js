3c82b0bf541ad7bc5c8399c49e0e36e4dfb27671
Matt Covey <covey.matt@gmail.com>
2026-08-13 16:12:02 -0500

    Cache contact connection update

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const CACHE_NAME = "blobbo-v3";
+const CACHE_NAME = "blobbo-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./menu.css",
   "./adventure.html",
   "./adventure.css",
+  "./connection.css",
   "./update-banner.css",
   "./adventure.js",
   "./arena.html",
