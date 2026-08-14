cf092c51df84831ba4737cc27b09db7bc0f9433c
Matt Covey <covey.matt@gmail.com>
2026-08-13 23:22:54 -0500

    Auto-activate critical PWA recovery

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,11 @@ const APP_SHELL = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", event => {
